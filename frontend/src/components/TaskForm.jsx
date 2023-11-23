760283868496eedec8c37d2f51a9bda5de4078cf
Daniel Suarez <est.daniel.suarez1@unimilitar.edu.co>
2023-11-23 16:02:06 +0000

    Interfaz de formulario para creacion y modificacion de fallas

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -85,94 +85,132 @@ const TaskForm = () => {
     setTask({ ...task, [e.target.name]: e.target.value });
 
   return (
-    <div className="h-[calc(100vh-64px)] flex flex-col items-center justify-center">
+    <div className="h-[calc(100vh-64px)] flex flex-col items-center justify-center pt-20">
       <form onSubmit={handleSubmit} className="w-2/5">
-        <h3 className="font-bold text-2xl my-3 text-white">
-          {params.id ? "Update Task" : "Create Task"}
+        <h3 className="font-bold text-2xl my-3 text-white text-center">
+            {params.id ? "Actualizar Falla" : "Publicar Falla"}
         </h3>
-        <input
-          type="number"
-          name="id_falla"
-          placeholder= {"Id Falla"}
-          className="border border-gray-400 p-2 rounded-md block my-2 w-full"
-          onChange={handleChange}
-          value={task.id_falla}
-          autoFocus
-        />
-        <input
-          type="number"
-          name="id_equipotel"
-          placeholder= {"Id Equipo de Telecomunicaciones"}
-          className="border border-gray-400 p-2 rounded-md block my-2 w-full"
-          onChange={handleChange}
-          value={task.id_equipotel}
-          autoFocus
-        />
-        <textarea
-          name="descripcion"
-          rows={4}
-          placeholder="Escriba su descripcion"
-          className="border border-gray-400 p-2 rounded-md block my-2 w-full"
-          onChange={handleChange}
-          value={task.descripcion}
-        ></textarea>
 
-			<input
-          type="text"
-          name="severidad"
-          placeholder="Severidad"
-          className="border border-gray-400 p-2 rounded-md block my-2 w-full"
-          onChange={handleChange}
-          value={task.severidad}
-          autoFocus
-        />
-			<input
-          type="number"
-          name="costo"
-          placeholder="Costo"
-          className="border border-gray-400 p-2 rounded-md block my-2 w-full"
-          onChange={handleChange}
-          value={task.costo}
-          autoFocus
-        />
-			<input
-          type="text"
-          name="estado"
-          placeholder="Estado del equipo"
-          className="border border-gray-400 p-2 rounded-md block my-2 w-full"
-          onChange={handleChange}
-          value={task.estado}
-          autoFocus
-        />
-			<input
-          type="date"
-          name="fecha_deteccion"
-          placeholder="Fecha de detección"
-          className="border border-gray-400 p-2 rounded-md block my-2 w-full"
-          onChange={handleChange}
-          value={task.fecha_deteccion}
-          autoFocus
-        />
-			<input
-          type="date"
-          name="fecha_finalizacion"
-          placeholder="Fecha de finalización"
-          className="border border-gray-400 p-2 rounded-md block my-2 w-full"
-          onChange={handleChange}
-          value={task.fecha_finalizacion}
-          autoFocus
-        />
-			<input
-          type="integer"
-          name="cedula"
-          placeholder="Cedula Tecnico"
-          className="border border-gray-400 p-2 rounded-md block my-2 w-full"
-          onChange={handleChange}
-          value={task.cedula}
-          autoFocus
-        />
+        <label className="flex justify-evenly"> 
+          <span className="w-full m-2 h-auto text-center p-3 text-white border border-solid rounded-md  ">Id Falla</span>
+          <input
+            type="number"
+            name="id_falla"
+            placeholder= "Id Falla"
+            className="border border-gray-400 p-2 rounded-md block my-2 w-full"
+            onChange={handleChange}
+            value={task.id_falla}
+            autoFocus
+          />
+        </label>
 
-        <div className="flex justify-between">
+        <label className="flex justify-evenly pb-2"> 
+          <span className="w-full m-2 h-auto text-center p-3 text-white border border-solid rounded-md  ">Id Equipo Teleco</span>
+          <input
+            type="number"
+            name="id_equipotel"
+            placeholder= {"Id Equipo de Telecomunicaciones"}
+            className="border border-gray-400 p-2 rounded-md block my-2 w-1/2"
+            onChange={handleChange}
+            value={task.id_equipotel}
+            autoFocus
+          />
+          
+        </label>
+
+        <label className="flex-col items-center justify-center pt-10"> 
+          <p className="w-full text-center text-white text-lg border border-solid rounded-md p-2">Escriba la descripción de la falla</p>
+          <textarea
+            name="descripcion"
+            rows={4}
+            placeholder="Escriba su descripcion"
+            className="border border-gray-400 p-2 rounded-md block my-2 w-full"
+            onChange={handleChange}
+            value={task.descripcion}
+          ></textarea>
+        </label>
+
+        <label className="flex justify-evenly"> 
+          <span className="w-1/3 m-2 h-auto text-center p-3 text-white border border-solid rounded-md ">Severidad</span>
+          <input
+            type="text"
+            name="severidad"
+            placeholder="Grave, Urgente, Ninguna, ..."
+            className="border border-gray-400 p-2 rounded-md block my-2 w-full"
+            onChange={handleChange}
+            value={task.severidad}
+            autoFocus
+          />
+        </label>
+
+        <label className="flex justify-evenly"> 
+          <span className="w-full m-2 h-auto text-center p-3 text-white border border-solid rounded-md ">Ingrese el costo de reparación</span>     
+          <input
+              type="number"
+              name="costo"
+              placeholder="Costo"
+              className="border border-gray-400 p-2 rounded-md block my-2 w-full"
+              onChange={handleChange}
+              value={task.costo}
+              autoFocus
+            />
+        </label>  
+
+        <label className="flex justify-evenly"> 
+          <span className="w-1/3 m-2 h-auto text-center p-3 text-white border border-solid rounded-md ">Estado del equipo</span>
+          <input
+              type="text"
+              name="estado"
+              placeholder="Dañado, reparado, en funcionamiento, ..."
+              className="border border-gray-400 p-2 rounded-md block my-2 w-full"
+              onChange={handleChange}
+              value={task.estado}
+              autoFocus
+            />
+        </label>
+
+        <label className="flex justify-evenly"> 
+          <span className="w-full m-2 h-auto text-center p-3 text-white border border-solid rounded-md  ">Fecha Descubrimiento</span>
+          <input
+              type="date"
+              name="fecha_deteccion"
+              placeholder="Fecha de detección"
+              className="border border-gray-400 p-2 rounded-md block my-2 w-full"
+              onChange={handleChange}
+              value={task.fecha_deteccion}
+              autoFocus
+            />
+        </label> 
+
+        <label className="flex justify-evenly"> 
+          <span className="w-full m-2 h-auto text-center p-3 text-white border border-solid rounded-md  ">Fecha Finalizacion</span>
+          <input
+              type="date"
+              name="fecha_finalizacion"
+              placeholder="Fecha de finalización"
+              className="border border-gray-400 p-2 rounded-md block my-2 w-full"
+              onChange={handleChange}
+              value={task.fecha_finalizacion}
+              autoFocus
+            />
+        </label>
+
+        <label className="flex justify-evenly"> 
+          <span className="w-full m-2 h-auto text-center p-3 text-white border border-solid rounded-md  ">Cedula Tecnico Encargado</span>
+          <input
+              type="integer"
+              name="cedula"
+              placeholder="Cedula Tecnico"
+              className="border border-gray-400 p-2 rounded-md block my-2 w-full"
+              onChange={handleChange}
+              value={task.cedula}
+              autoFocus
+            />
+        </label>
+
+        
+
+        <div className="flex justify-between p-10">
           <button
             type="submit"
 						disabled={
@@ -190,7 +228,7 @@ const TaskForm = () => {
             {loading
               ? // <CircularProgress color="inherit" size={25} />
                 loading
-              : "Save"}
+              : "Publicar"}
           </button>
 
           {params.id && (
@@ -198,7 +236,7 @@ const TaskForm = () => {
               className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
               onClick={() => handleDelete(params.id)}
             >
-              Delete
+              Eliminar
             </button>
           )}
         </div>
