ee06aa5b15446d766424c0b59db01dbd9f24fbf9
Daniel Suarez <est.daniel.suarez1@unimilitar.edu.co>
2023-11-23 16:18:58 +0000

    Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,9 +5,15 @@ export default function ButtonAppBar() {
   const location =  useLocation()
 
   return (
-    <nav className="flex items-center justify-between">
+    <nav className="flex items-center justify-between border-b pb-2">
       <Link to="/">
-        <h1 className="text-white font-bold text-4xl my-4">Gestión de Fallas y Mantenimiento en Infraestructura de Telecomunicaciones</h1>
+        <div className="flex mt-2">
+        <h1 className="text-white font-bold text-4xl my-4">FYMET</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 19.5v-.75a7.5 7.5 0 00-7.5-7.5H4.5m0-6.75h.75c7.87 0 14.25 6.38 14.25 14.25v.75M6 18.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+        </svg>
+        </div>
+        <span className="text-white ">Sistema de Gestión de Fallas y Mantenimiento de Equipos de Telecomunicaciones</span>
       </Link>
 
       {location.pathname === "/tasks/new" || location.pathname.includes('/tasks/') ? (
@@ -15,15 +21,23 @@ export default function ButtonAppBar() {
           className="bg-slate-200 text-black font-bold py-2 px-4 rounded-lg my-2"
           onClick={() => navigate("/")}
         >
-          Go back
+          Volver
         </button>
       ) : (
-        <button
-          className="bg-slate-200 text-black font-bold py-2 px-4 rounded-lg my-2"
-          onClick={() => navigate("/tasks/new")}
-        >
-          Add Task
-        </button>
+        <div>
+          <button
+            className="bg-slate-200 text-black font-bold py-2 px-4 mr-4 rounded-lg my-2 "
+            onClick={() => navigate("/tasks/new")}
+          >
+            Programar Mantenimiento
+          </button>
+          <button
+            className="bg-slate-200 text-black font-bold py-2 px-4 rounded-lg my-2"
+            onClick={() => navigate("/tasks/new")}
+          >
+            Añadir Falla
+          </button>
+        </div>
       )}
     </nav>
   );
